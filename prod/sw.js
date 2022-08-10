ab78a1d4a3985082619e31e264e7b6ff11461c37
quandangv <quandangv@gmail.com>
2022-08-10 07:48:29 +0700

    Make app available offline

--- prod/sw.js
+++ prod/sw.js
@@ -1,3 +1,21 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
+const cacheName = 'up';
+const cachedFiles = [
+  './up.html',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(cacheName)
+    await cache.addAll(cachedFiles)
+  })())
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    if (r) { return r; }
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
 });
