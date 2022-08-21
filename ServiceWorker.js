8142a1c196da0cda3e32c162145193102e934b38
quandangv <quandangv@gmail.com>
2022-08-21 18:31:02 +0700

    Improve service worker

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,8 +1,26 @@
-const cacheName = 'gizapp';
+const cacheName = 'gizappv1';
 
-self.addEventListener('install', event => {});
-self.addEventListener('fetch', (e) => {
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open("v1");
+  await cache.addAll(resources);
+  console.log('sw installed')
+};
+
+self.addEventListener("install", (event) => {
+  cachePaths = new URL(location).searchParams.get('cachePaths').split(';')
+  console.log(cachePaths)
+  event.waitUntil(
+    addResourcesToCache(cachePaths)
+  );
+  console.log('sw installed')
+});
+self.addEventListener("activate" , e => {
+  e.waitUntil(clients.claim())
+});
+self.addEventListener('fetch', e => {
+  console.log(e.request)
   e.respondWith((async () => {
+    console.log(e.request)
     const r = await caches.match(e.request);
     if (r) { return r; }
     const response = await fetch(e.request);
